bc68482a72d0796813aca1a81df1c636c1ecc35d
Aditya <147165909+ADiTyaRaj8969@users.noreply.github.com>
2025-12-02 21:26:33 +0530

    Add files via upload

---
+++ 19. Pointer difference demo.py
@@ -0,0 +1,14 @@
+"""
+Topic: Pointer difference simulated via indices
+"""
+def main():
+    arr = [10,20,30,40,50]
+    p1 = 1
+    p2 = 4
+    print('Elements between:', p2 - p1)
+
+if __name__ == '__main__':
+    main()
+
+# Time Complexity: O(1).
+# Space Complexity: O(1).
